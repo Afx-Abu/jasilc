56a2facbaa90df9f09831c6e200f26cafa5a4785
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-05-08 16:13:27 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCFQxFL4hhEsiBjhnuvUKhw2LjD26_C0jY",
+  authDomain: "jasil-ba4fa.firebaseapp.com",
+  projectId: "jasil-ba4fa",
+  storageBucket: "jasil-ba4fa.appspot.com",
+  messagingSenderId: "193413057909",
+  appId: "1:193413057909:web:ef4e9ddcaa05eeab03173c",
+  measurementId: "G-ES0GFBPZNK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
